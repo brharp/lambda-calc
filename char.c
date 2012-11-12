f4b66277caf3b4dae98fa3ec10c847c007f72208
M. Brent Harp <brharp@uoguelph.ca>
2012-11-12 12:26:23 -0500

    Added character constants

---
+++ char.c
@@ -0,0 +1,16 @@
+
+#include <wchar.h>
+
+ /* Constants */
+
+const wint_t lambda    = L'\\';
+const wint_t separator = L'.';
+const wint_t lparen    = L'(';
+const wint_t rparen    = L')';
+const wint_t quote     = L'\'';
+const wint_t space     = L' ';
+const wint_t assign    = L'=';
+const wint_t tab       = L'\t';
+const wint_t newline   = L'\n';
+const wint_t comment   = L';';
+
---
+++ char.h
@@ -0,0 +1,29 @@
+#ifndef _CHAR_H_INCLUDED_
+#define _CHAR_H_INCLUDED_
+#include <wchar.h>
+/*++
+/* NAME
+/*	char 3h
+/* SUMMARY
+/*	Character functions and constants.
+/* DESCRIPTION
+/* .nf
+
+ /* Constants */
+
+extern const wint_t lambda;
+extern const wint_t separator;
+extern const wint_t lparen;
+extern const wint_t rparen;
+extern const wint_t quote;
+extern const wint_t space;
+extern const wint_t assign;
+extern const wint_t tab;
+extern const wint_t newline;
+extern const wint_t comment;
+
+/* AUTHOR
+/*	Brent Harp
+/*--*/
+#endif
+
